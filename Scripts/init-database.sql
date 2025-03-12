932eba6c50b769ddf6a0ae2f0cb305a0d798d55f
Karthik131223 <nutcracker.me.69@gmail.com>
2025-03-12 15:20:38 +0530

    Create init-database.sql

---
+++ Scripts/init-database.sql
@@ -0,0 +1,16 @@
+-- Create Database "Datawarehouse"
+
+Use master;
+
+CREATE DATABASE Datawarehouse;
+
+Use Datawarehouse;
+
+-- Create schema for each layer
+
+CREATE SCHEMA Bronze;
+
+CREATE SCHEMA Silver;
+
+CREATE SCHEMA Gold;
+
